8ae854bc31096f3e15831321d45fd6978782082a
RickFerry <rick_ferry1@hotmail.com>
2025-12-25 11:48:42 -0300

    Initial commit

---
+++ src/test/java/com/ferry/access/AccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferry.access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
