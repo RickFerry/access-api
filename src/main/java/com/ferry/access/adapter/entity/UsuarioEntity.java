ce2d1bfb4e8fccdef8260f58515163656f9ae06f
Ricardo <rick_ferry1@hotmail.com>
2025-12-25 21:01:00 -0300

    fix: add unique constraint to email field in UsuarioEntity

--- src/main/java/com/ferry/access/adapter/entity/UsuarioEntity.java
+++ src/main/java/com/ferry/access/adapter/entity/UsuarioEntity.java
@@ -16,9 +16,13 @@ public class UsuarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String email;
+
     private String senha;
     private Boolean administrador;
+
     @OneToOne
     @JoinColumn(name = "pessoa_id")
     private PessoaEntity pessoa;
